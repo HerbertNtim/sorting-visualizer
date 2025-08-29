908e5478508bc54186426a10fb29419c614e667a
HerbertNtim <herbertntim15@gmail.com>
2025-08-29 16:37:33 +0000

    interface

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-let n = 10
-const array = []
+let n = 10;
+const array = [];
 
-for (let i = 0; i <= n; i++) {
-  array[i] = Math.random();
+init()
+
+function init() {
+  for (let i = 0; i <= n; i++) {
+    array[i] = Math.random();
+  }
+
+  showBars();
 }
 
-console.log(array)
+function play() {
+  bubbleSort(array)
+  showBars()
+}
 
-for (let i = 0; i < array.length; i++) {
-  const bar = document.createElement('div');
-  bar.style.height = array[i] * 100 + '%';
-  bar.classList.add('bar')
-  container.appendChild(bar)  
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j + 1] > array[j]) {
+        const temp = array[j];
+        array[j + 1] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+
+  return array
+}
+
+function showBars() {
+  container.innerHTML = "";
+  for (let i = 0; i < array.length; i++) {
+    const bar = document.createElement("div");
+    bar.style.height = array[i] * 100 + "%";
+    bar.classList.add("bar");
+    container.appendChild(bar);
+  }
 }
