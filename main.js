f78306f6a8127fc9339582de6571db5198df23fe
HerbertNtim <herbertntim15@gmail.com>
2025-09-01 16:14:48 +0000

    implemented animation

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let n = 10;
 const array = [];
 
-init()
+init();
 
 function init() {
-  for (let i = 0; i <= n; i++) {
+  for (let i = 0; i < n; i++) {
     array[i] = Math.random();
   }
 
@@ -12,30 +12,66 @@ function init() {
 }
 
 function play() {
-  bubbleSort(array)
-  showBars()
+  const copy = [...array]
+  const swaps = bubbleSort(copy);
+  animate(swaps)
+}
+
+function animate(swaps) {
+  if (swaps.length === 0) {
+    return;
+  }
+
+  const [i, j] = swaps.shift();
+  [array[i], array[j]] = [array[j], array[i]];
+  showBars([i, j]);
+  setTimeout(() => {
+    animate(swaps);
+  }, 500);
 }
 
 function bubbleSort(array) {
+  const swaps = [];
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if (array[j + 1] > array[j]) {
-        const temp = array[j];
-        array[j + 1] = array[j];
-        array[j] = temp;
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swaps.push([j, j + 1]);
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
   }
 
-  return array
+  return swaps;
 }
 
-function showBars() {
+// function bubbleSort(array) {
+//   const swaps = [];
+//   do {
+//     var swapped = false;
+//     for (let i = 1; i < array.length; i++) {
+//       if (array[i - 1] > array[i]) {
+//         swapped = true;
+//         swaps.push(i - 1, i);
+//         [array[i - 1], array[i]] = [
+//           array[i],
+//           array[i - 1],
+//         ];
+//       }
+//     }
+//   } while (swapped);
+
+//   return swaps;
+// }
+
+function showBars(indices) {
   container.innerHTML = "";
   for (let i = 0; i < array.length; i++) {
     const bar = document.createElement("div");
     bar.style.height = array[i] * 100 + "%";
     bar.classList.add("bar");
+    if(indices && indices.includes(i)) {
+      bar.style.backgroundColor = 'red'
+    }
     container.appendChild(bar);
   }
 }
