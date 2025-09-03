87656c29f65bd73bba1f557d1a6ae1ae9e521437
HerbertNtim <herbertntim15@gmail.com>
2025-09-03 12:03:24 +0000

    finished animation

--- main.js
+++ main.js
@@ -12,65 +12,51 @@ function init() {
 }
 
 function play() {
-  const copy = [...array]
-  const swaps = bubbleSort(copy);
-  animate(swaps)
+  const copy = [...array];
+  const moves = bubbleSort(copy);
+  animate(moves);
 }
 
-function animate(swaps) {
-  if (swaps.length === 0) {
+function animate(moves) {
+  if (moves.length === 0) {
+    showBars();
     return;
   }
 
-  const [i, j] = swaps.shift();
-  [array[i], array[j]] = [array[j], array[i]];
-  showBars([i, j]);
+  const move = moves.shift();
+  const [i, j] = move.indices;
+  if (move.type === "swap") {
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  showBars(move);
   setTimeout(() => {
-    animate(swaps);
+    animate(moves);
   }, 500);
 }
 
 function bubbleSort(array) {
-  const swaps = [];
+  const moves = [];
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
+      moves.push({ indices: [j, j + 1], type: "comp" });
       if (array[j] > array[j + 1]) {
-        swaps.push([j, j + 1]);
+        moves.push({ indices: [j, j + 1], type: "swap" });
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
   }
 
-  return swaps;
+  return moves;
 }
 
-// function bubbleSort(array) {
-//   const swaps = [];
-//   do {
-//     var swapped = false;
-//     for (let i = 1; i < array.length; i++) {
-//       if (array[i - 1] > array[i]) {
-//         swapped = true;
-//         swaps.push(i - 1, i);
-//         [array[i - 1], array[i]] = [
-//           array[i],
-//           array[i - 1],
-//         ];
-//       }
-//     }
-//   } while (swapped);
-
-//   return swaps;
-// }
-
-function showBars(indices) {
+function showBars(move) {
   container.innerHTML = "";
   for (let i = 0; i < array.length; i++) {
     const bar = document.createElement("div");
     bar.style.height = array[i] * 100 + "%";
     bar.classList.add("bar");
-    if(indices && indices.includes(i)) {
-      bar.style.backgroundColor = 'red'
+    if (move && move.indices.includes(i)) {
+      bar.style.backgroundColor = move.type === 'swap' ? "red" : 'blue';
     }
     container.appendChild(bar);
   }
